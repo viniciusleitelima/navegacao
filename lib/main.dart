e0fbd2007651a82403b383df6970eeaedb8c3cc7
Vinicius Lima <viniciusleite.desenvolvedor@gmail.com>
2019-09-30 15:45:55 -0300

    inital commit'

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:navegacao/TelaSecundaria.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: TelaPrincipal(),
+  ));
+}
+
+class TelaPrincipal extends StatefulWidget {
+  @override
+  _TelaPrincipalState createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Principal"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text("Ir para a segunda tela"),
+              padding: EdgeInsets.all(15),
+              onPressed: (){
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => TelaSecundaria()
+                    )
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
